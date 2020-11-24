8d5f59f0a9917681317986479d4a630ee4716bef
Matt Riley <mattrileyaus@gmail.com>
2020-11-24 22:57:40 +1100

    narrow results to first basepath

--- src/lib/find-duplicates-async.js
+++ src/lib/find-duplicates-async.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+// filters to ensure only focused on basepaths[0]
+
 module.exports = ({ lib, globby }) => async basepaths => {
 
     const files = await basepaths.reduce(async (p, basepath, i) => {
@@ -14,14 +16,20 @@ module.exports = ({ lib, globby }) => async basepaths => {
     }, []);
 
     files.sort((a, b) => a.stats.size - b.stats.size);
-    const groups = lib.groupBySize(files).filter(files => files.length > 1);
+    const groups = lib.groupBySize(files).filter(files => {
+        return files.length > 1 && files.some(f => f.basepathIndex === 0);
+    });
+
+
     const duplicates = await lib.findDuplicatesInGroupsAsync(groups);
 
-    return duplicates.map(files => {
-        return files.map(f => {
-            const { basepathIndex, relpath } = f;
-            return { basepathIndex, relpath };
+    return duplicates
+        .filter(files => files.some(f => f.basepathIndex === 0))
+        .map(files => {
+            return files.map(f => {
+                const { basepathIndex, relpath } = f;
+                return { basepathIndex, relpath };
+            });
         });
-    });
 
 };
