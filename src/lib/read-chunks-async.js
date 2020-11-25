9967c99b0944fbff96b77ab957217809ce702f49
Matt Riley <mattrileyaus@gmail.com>
2020-11-25 23:20:30 +1100

    remove the need to track file position

--- src/lib/read-chunks-async.js
+++ src/lib/read-chunks-async.js
@@ -2,11 +2,10 @@ module.exports = ({ lib, config }) => files => {
 
     return Promise.all(files.map(async f => {
         f = await lib.openFileAsync(f);
-        const res = await f.handle.read(f.buffer, 0, config.step, f.pos);
-        const pos = f.pos + res.bytesRead;
-        const done = pos === f.stats.size;
+        const res = await f.handle.read(f.buffer, 0, config.step, null);
+        const done = res.bytesRead === 0;
         if (done) lib.closeFiles([f]);
-        return { ...f, pos, done };
+        return { ...f, done };
     }));
     
 };
--- tests/find-duplicates.test.js
+++ tests/find-duplicates.test.js
@@ -8,15 +8,15 @@ module.exports = ({ test }) => {
         const duplicates = await findDuplicates({ sourcePath, searchPaths });
         t.equal(duplicates, [
             [
-                'test-files/source-path/A-2-201KB.JPG',
                 'test-files/source-path/A-1-201KB.JPG',
+                'test-files/source-path/A-2-201KB.JPG',
                 'test-files/source-path/A/A-3-201KB.JPG',
                 'test-files/search-path-1/A-3-201KB.JPG',
                 'test-files/search-path-2/A-3-201KB.JPG'
             ],
             [
-                'test-files/source-path/B/B-2-38KB.JPG',
-                'test-files/source-path/B-1-38KB.JPG'
+                'test-files/source-path/B-1-38KB.JPG',
+                'test-files/source-path/B/B-2-38KB.JPG'
             ]
         ]);
     });
