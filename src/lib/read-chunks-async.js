563fed95210bcb78c8d668dd99061942898b1783
Matt Riley <mattrileyaus@gmail.com>
2020-11-26 19:06:27 +1100

    track file position

--- src/lib/read-chunks-async.js
+++ src/lib/read-chunks-async.js
@@ -3,10 +3,11 @@ module.exports = ({ lib, config }) => files => {
     return Promise.all(files.map(async f => {
         try {
             f = await lib.openFileAsync(f);
-            const { bytesRead } = await f.handle.read(f.buffer, 0, config.step, null);
+            const { bytesRead } = await f.handle.read(f.buffer, 0, config.step, f.pos);
+            const pos = f.pos + bytesRead;
             const done = bytesRead === 0;
             if (done) await lib.closeFilesAsync([f]);
-            return { ...f, done };
+            return { ...f, pos, done };
         } catch (err) {
             err.context = { f };
             throw err;
