03d199a39293232b2491a958c221a795f2d243a5
Matt Riley <mattrileyaus@gmail.com>
2020-11-29 00:12:40 +1100

    refactor

--- src/lib/group-by-buffer.js
+++ src/lib/group-by-buffer.js
@@ -3,30 +3,27 @@ module.exports = () => files => {
     const flatmap = new Map();
     const maps = [];
 
-    const addMap = () => {
+    const newMap = () => {
         const map = new Map();
         maps.push(map);
         return map;
     };
 
-    const addToMap = (a, b) => {
-        const map = maps.find(map => map.has(a.path) || map.has(b.path)) || addMap();
-        map.set(a.path, a).set(b.path, b);
-        flatmap.set(a.path, a).set(b.path, b);
+    const push = (...files) => {
+        const findMap = () => maps.find(map => files.some(f => map.has(f.path)));
+        const map = files.length > 1 ? findMap() || newMap() : newMap();
+        files.forEach(f => [map, flatmap].forEach(map => map.set(f.path, f)));
     };
 
     for (let i = 0; i < files.length; i++) {
         for (let j = i + 1; j < files.length; j++) {
             const a = files[i];
             const b = files[j];
-            const same = Buffer.compare(a.buffer, b.buffer) === 0;
-            if (same) addToMap(a, b);
+            if (Buffer.compare(a.buffer, b.buffer) === 0) push(a, b);
         }
     }
 
-    files.forEach(f => {
-        if (!flatmap.has(f.path)) addMap().set(f.path, f);
-    });
+    files.forEach(f => !flatmap.has(f.path) && push(f));
 
     return maps.map(map => [...map.values()]);
     
