901395d16525216eb6cbe919cb17eb3272d381bc
Matt Riley <mattrileyaus@gmail.com>
2020-11-29 13:01:49 +1100

    refactor

--- src/lib/read-chunks-async.js
+++ src/lib/read-chunks-async.js
@@ -2,7 +2,6 @@ module.exports = ({ lib }) => getStrategy => files => {
 
     return Promise.all(files.map(async f => {
         try {
-            if (f.done) return f;
             f = await lib.openFileAsync(f, getStrategy);
             const { length, position } = f.strategy.next(f);
             const readResult = await f.handle.read(f.buffer, 0, length, position);
