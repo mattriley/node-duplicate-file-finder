f2fbc5ce27e46d296f11d36ea92ea0540dc75e7d
Matt Riley <mattrileyaus@gmail.com>
2020-11-30 23:41:52 +1100

    allow buffer size to be specified for default strategy

--- src/commands/find-duplicates.js
+++ src/commands/find-duplicates.js
@@ -1,13 +1,13 @@
-const defaults = { sourcePath: '.', searchPaths: [], objectMode: false };
+const defaults = { sourcePath: '.', searchPaths: [], objectMode: false, bufferSize: 8192 };
 
 module.exports = ({ strategies, lib }) => async args => {
 
-    const { sourcePath, searchPaths, objectMode } = { ...defaults, ...args };
+    const { sourcePath, searchPaths, objectMode, bufferSize } = { ...defaults, ...args };
     const basepaths = [sourcePath, ...searchPaths];
     const files = await lib.getFiles(basepaths);
     const filterPredicate = files => files.length > 1 && files.some(f => f.basepath === sourcePath);
     const groupedBySize = lib.groupBySize(files, filterPredicate);
-    const createDefaultStrategy = strategies.whole({ bufferSize: 8192 });
+    const createDefaultStrategy = strategies.whole({ bufferSize });
     const createStrategy = args.createStrategy || createDefaultStrategy;
     const readChunksAsync = lib.readChunksAsync(createStrategy);
     const groupedByContent = await lib.findDuplicatesInGroupsAsync(groupedBySize, readChunksAsync, filterPredicate);
