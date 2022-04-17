1c2d61c539352e587c69ea1342eb9dccc752005d
Matt Riley <mattrileyaus@gmail.com>
2022-04-17 20:24:05 +1000

    remove mkdirp

--- src/boot.js
+++ src/boot.js
@@ -1,6 +1,5 @@
 const modules = require('./modules');
 const fs = require('fs');
-const mkdirp = require('mkdirp');
 const glob = require('fast-glob');
 const composer = require('module-composer');
 
@@ -9,7 +8,7 @@ module.exports = (overrides = {}) => {
     const compose = composer(modules, { overrides });
 
     const strategies = compose('strategies');
-    const lib = compose('lib', { strategies, fs, glob, mkdirp });
+    const lib = compose('lib', { strategies, fs, glob });
     compose('commands', { strategies, lib });
 
     return compose.modules;
