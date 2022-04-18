02cda26aed4ebecffafb5bc06667085538f559a6
Matt Riley <mattrileyaus@gmail.com>
2022-04-18 14:12:13 +1000

    Unspecified changes

--- src/boot.js
+++ src/boot.js
@@ -4,12 +4,9 @@ const modules = require('./modules');
 module.exports = (overrides = {}) => {
 
     const compose = composer(modules, { overrides });
-
-    const strategies = compose('strategies');
-    const io = compose('io', {}, io => io.setup());
-    const lib = compose('lib', { strategies, io });
-    compose('commands', { strategies, lib });
-
-    return compose.modules;
+    const { strategies } = compose('strategies');
+    const { io } = compose('io', {}, io => io.setup());
+    const { lib } = compose('lib', { strategies, io });
+    return compose('commands', { strategies, lib });
 
 };
